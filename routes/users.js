b7d09e8749483acf664b76b88509befc593cf27c
iamdudumon <kksy4511@naver.com>
2023-11-28 16:35:53 +0900

    feat: user router에 이메일 중복 여부를 반환 하는 api 구현

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const User = require("../model/User");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/email/:email", async function (req, res, next) {
+  const email = req.params.email;
+  const emailExist = await User.getEmail(email);
+
+  if (emailExist) res.sendStatus(409);
+  else res.sendStatus(200);
 });
 
 module.exports = router;
