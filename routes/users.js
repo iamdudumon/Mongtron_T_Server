112adf75b9614ed9bc76399c7a5bed71d6870b9c
iamdudumon <kksy4511@naver.com>
2023-11-28 17:41:38 +0900

    feat: 로그인 api 구현 (1/2)

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const bcrypt = require("bcrypt");
 
 const User = require("../model/User");
 
@@ -10,6 +11,20 @@ router.get("/email/:email", async function (req, res, next) {
 
   if (emailExist) res.sendStatus(409);
   else res.sendStatus(200);
+});
+
+router.post("/login", async function (req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.getUser(email, password);
+
+  // email이 틀리거나, 비밀번호가 틀릴 시
+  if (user == [] || !bcrypt.compareSync(password, user[0].password)) {
+    res.sendStatus(401);
+  } else {
+    res.status(200).json(user);
+  }
 });
 
 module.exports = router;
