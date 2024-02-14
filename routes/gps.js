64562c37dfb82f5dc4f0939b841060f514df482d
iamdudumon <kksy4511@naver.com>
2024-02-14 15:56:11 +0900

    feat: gps router 및 service 추가

--- routes/gps.js
+++ routes/gps.js
@@ -2,19 +2,59 @@ var express = require("express");
 var router = express.Router();
 
 const User = require("../service/User");
+const Gps = require("../service/Gps");
 
-router.patch("/", async (req, res, next) => {
-  const id = req.body.id;
+router.patch("/:id", async (req, res, next) => {
+  const id = req.params.id;
   const latitude = req.body.latitude;
   const longitude = req.body.longitude;
   const radiusInfo = req.body.radiusInfo;
 
-  const result = await User.updateGpsCoordinate(
+  const result = await Gps.updateGpsCoordinate(
     id,
     latitude,
     longitude,
     radiusInfo
   );
+
+  if (result) res.sendStatus(200);
+  else res.sendStatus(503);
+});
+
+router.get("/position", async (req, res, next) => {
+  const id = req.query.id;
+  const lat = req.query.latitude;
+  const lon = req.query.longitude;
+  const radiusInfo = req.query.radiusInfo;
+
+  const others = await Gps.getNearbyUsersLocation(id, lat, lon, radiusInfo);
+
+  if (lat == undefined || lon == undefined || radiusInfo == undefined) {
+    res.sendStatus(400);
+  }
+
+  let list = [];
+  others.forEach((row) => {
+    let response = new Object();
+    response.id = row.id;
+    response.nickName = row.nickName;
+    response.age = row.age;
+    response.sex = row.sex == "male" ? "1" : "2";
+    response.gpsState = row.gpsState == "1" ? true : false;
+    response.latitude = row.latitude;
+    response.longitude = row.longitude;
+    response.distance = row.distance;
+    list.push(response);
+  });
+
+  res.status(200).json({ users: list, length: others.length });
+});
+
+router.patch("/state/:id", async (req, res) => {
+  const id = req.params.id;
+  const gpsState = req.body.gpsState == "true" ? "1" : "0";
+
+  const result = await Gps.updateGpsState(id, gpsState);
 
   if (result) res.sendStatus(200);
   else res.sendStatus(503);
---
+++ service/Gps.js
@@ -0,0 +1,44 @@
+const db = require("../util/db");
+
+async function updateGpsCoordinate(id, latitude, longitude) {
+  const sql = "update user set latitude =? , longitude = ? where id = ?";
+
+  try {
+    await db.query(sql, [id, latitude, longitude]);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+async function getNearbyUsersLocation(id, lat, lon, radiusInfo) {
+  const sql =
+    "SELECT " +
+    "id, nickName, age, sex, gpsState, latitude, longitude," +
+    "(6371 * acos ( cos ( radians(?) )" +
+    "* cos( radians( latitude ) )" +
+    "* cos( radians(longitude) - radians(?) )" +
+    "+ sin ( radians(?) ) * sin( radians(latitude) )" +
+    ")) AS distance " +
+    "from user where gpsState = '1' and not id = ? having distance < ?";
+
+  const others = await db.query(sql, [lat, lon, lat, id, radiusInfo]);
+  return others;
+}
+
+async function updateGpsState(id, gpsState) {
+  const sql = "update user set gpsState = ? where id = ?";
+
+  try {
+    await db.query(sql, [gpsState, id]);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+module.exports = {
+  updateGpsCoordinate,
+  getNearbyUsersLocation,
+  updateGpsState,
+};
