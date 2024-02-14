4fee4b571b0ea1a05e31cc9e06d8104cce43182a
iamdudumon <kksy4511@naver.com>
2024-02-14 16:37:07 +0900

    feat: friend 추가 api 구현

--- routes/friend.js
+++ routes/friend.js
@@ -3,4 +3,12 @@ var router = express.Router();
 
 const Friend = require("../service/Friend");
 
+router.post("/", async (req, res, next) => {
+  const { myId, friendId } = req.query;
+  const result = await Friend.insertFriend(myId, friendId);
+
+  if (result) res.sendStatus(200);
+  else res.sendStatus(409);
+});
+
 module.exports = router;
---
+++ service/Friend.js
@@ -0,0 +1,17 @@
+const db = require("../util/db");
+
+async function insertFriend(myId, friendId) {
+  const sql = "inset into friend(pre_id, post_id) values(?, ?)";
+
+  try {
+    await db.query(sql, [myId, friendId]);
+    return true;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
+}
+
+module.exports = {
+  insertFriend,
+};
